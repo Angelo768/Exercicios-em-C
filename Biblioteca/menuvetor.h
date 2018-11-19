1cc694591c930051570166b90926d83471074ffa
Angelo <angelo.sousa@acenjr.com>
2018-11-19 14:39:59 -0300

    Falta validar as entradas dos vetores

--- Biblioteca/menuvetor.h
+++ Biblioteca/menuvetor.h
@@ -1,10 +1,10 @@
-/* #############################################################
-    TRABALHO DA CADEIRA DE INTRODUÇÃO A CIÊNCIA DA COMPUTAÇÃO
-         BIBLIOTECA DE MANIPULAÇÃO DE VETORES EM C
-            Autor: Raimundo Angeliano Gonçalves de Sousa
-               UNIVERSIDADE ESTADUAL DO CEÁRA
-                   FORTALEZA/CE - 2018
-################################################################*/
+/*#################################################################
+  #    TRABALHO DA CADEIRA DE INTRODUÇÃO A CIÊNCIA DA COMPUTAÇÃO  #
+  #         BIBLIOTECA DE MANIPULAÇÃO DE VETORES EM C             #
+  #            Autor: Raimundo Angeliano Gonçalves de Sousa       #
+  #               UNIVERSIDADE ESTADUAL DO CEÁRA                  #
+  #                   FORTALEZA/CE - 2018                         #
+  #################################################################*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,9 +15,9 @@ int lervetor(int **v1, int **v2, int *t);
 int Somar(int *v1, int *v2, int t);
 int Mult(int *v1, int *v2, int t);
 int Prod2V(int *v1, int *v2, int t);
-int linhap(int l){
+int linhavet(int l){
     for(int i = 0; i < l; i++){
-        printf("=");
+        printf("-");
     }
     printf("\n");
 }
@@ -33,17 +33,17 @@ int voltavet(){
 int lervetor(int **v1, int **v2, int *t){
 
     int a, i;
-    printf("-------------------------\n");
+    linhavet(30);
     printf("Tamanho dos vetores: ");
     scanf("%d", t);
-    printf("-------------------------\n");
+    linhavet(30);
     *v1 = (int*)malloc((*t)*sizeof(int));
     *v2 = (int*)malloc((*t)*sizeof(int));
     for ( i = 0;  i < *t; i++){
         printf("Vetor 1 - Posição [%d]: ", i);
         scanf("%d", &(*v1)[i]);
     }
-    printf("-------------------------\n");
+    linhavet(30);
     for ( i = 0; i < *t; i++){
         printf("Vetor 2 - Posição [%d]: ", i);
         scanf("%d", &(*v2)[i]);
@@ -56,8 +56,9 @@ int lervetor(int **v1, int **v2, int *t){
 int Somar(int *v1, int *v2, int t){
     printf("\n");
     int p;
+    linhavet(30);
     for (int i = 0; i < t; i++) printf("v1[%d] + v2[%d] = [%d]\n",v1[i], v2[i], v1[i]+v2[i]);
-    printf("\n\n");
+    linhavet(30);
     voltavet();
 }
 
@@ -65,7 +66,7 @@ int Somar(int *v1, int *v2, int t){
 // FUNÇÃO PARA MULTIPLICAR POR UM ESCALAR
 int Mult(int *v1, int *v2, int t){
     printf("\n");
-    printf("===========================\n");
+    linhavet(30);
     printf("[1] Vetor1: ");
     for (int i=0;i<t;i++){
         printf("%d ", v1[i]);
@@ -76,33 +77,33 @@ int Mult(int *v1, int *v2, int t){
         printf("%d ", v2[i]);
   }
     printf("\n");
-    printf("===========================\n");
+    linhavet(30);
     int escolha, escalar, voltar;
     printf("\nQual dos vetores você quer multiplicar? ");
     scanf("%d", &escolha);
   switch (escolha){
     case 1:{
       printf("\nQual o escalar?");
-      scanf("%d", &escalar);
       while(escalar != 0){
-          if (escalar == 0) break;
-          for(int i=0;i<t;i++) printf("v1[%d] x v2[%d] = [%d]\n", v1[i], escalar, v1[i]*escalar); break;
-      } voltavet(); break;
+          scanf("%d", &escalar);
+          linhavet(30);
+          for(int i=0;i<t;i++) printf("v1[%d] x %d = [%d]\n", v1[i], escalar, v1[i]*escalar); break;
+      }   linhavet(30); voltavet(); break;
 
   } break;
     case 2:{
       printf("\nQual o escalar? ");
-      scanf("%d", &escalar);
       while(escalar != 0){
-          if (escalar == 0) break;
-          for(int i=0;i<t;i++) printf("v1[%d] x v2[%d] = [%d]\n", v2[i], escalar, v2[i]*escalar); break;
-      } voltavet(); break;
+          scanf("%d", &escalar);
+          linhavet(30);
+          for(int i=0;i<t;i++) printf("v1[%d] x %d = [%d]\n", v2[i], escalar, v2[i]*escalar); break;
+      } linhavet(30); voltavet(); break;
 
   } break;
     default: {
-        printf("==================================================\n");
-        printf("Opção inválida           \n"); voltavet();
-        printf("==================================================\n");
+        linhavet(40);
+        printf("Opção inválida "); voltavet();
+        linhavet(40);
     }
 
   }
@@ -112,10 +113,10 @@ int Mult(int *v1, int *v2, int t){
 
 // FUNÇÃO QUE RETORNA O PRODUTO ENTRE DOIS VETORES LIDOS PELA FUNÇÃO lervetor();
 int Prod2V(int *v1, int *v2, int t){
-    printf("============================\n");
+    linhavet(20);
     for(int i = 0; i < t; i++){
         printf("v1[%d] * v2[%d] = [%d]\n", v1[i], v2[i], v1[i]*v2[i]);
     }
-    printf("============================\n");
+    linhavet(20);
     voltavet();
 }
