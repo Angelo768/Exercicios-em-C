158c7394b4150e8a9b17470449493f42b398204f
Angelo <angelo.sousa@acenjr.com>
2018-11-19 13:09:24 -0300

    Função vetor completed

--- Biblioteca/biblioteca.c
+++ Biblioteca/biblioteca.c
@@ -5,49 +5,58 @@
 int tam = 1, *vector1 = NULL , *vector2 = NULL;
 int emv=0, emp=0;
 
+int linhap(int l){
+    for(int i = 0; i < l; i++){
+        printf("=");
+    }
+    printf("\n");
+}
+
+// MENU PRINCIPAL DA BIBLIOTECA DE VETORES E MATRIZES
 int main(){
-    int op;
-    //printf("\e[H\e[2J");
-    printf("==============================\n");
+    int op, voltar;
+    printf("\e[H\e[2J");
+    linhap(30);
     printf("|\tMenu Inicial         |\n");
-    printf("==============================\n");
+    linhap(30);
     printf("|1. Operações com vetores    |\n");
     printf("|2. Operações com matrizes   |\n");
-    printf("|3. Sair do programa         |\n\n");
-    printf("Operação desejada: ");
-    scanf("%d", &op);
+    printf("|3. Sair do programa         |\n");
+    linhap(30); printf("\n");
+    printf("Operação desejada: "); scanf("%d", &op);
+
     switch (op){
         case 1: menuvetor(); break;
         case 2: menumatriz(); break;
         case 3: printf("Encerrando programa...\n"); break;
-        default: printf("Opção inválida... Tente uma das opções do menu!\n\n"); main();
+        default:{
+            printf("\nOpção inválida... Tente uma das opções do menu!\n\n0 para voltar ao menu: ");
+            scanf("%d", &voltar); main();
+        }
     }
-
-    emp++;
 }
 
 
 int menuvetor(){
     int mv;
-
     printf("\e[H\e[2J");
-    printf("===================================================\n");
+    linhap(51);
     printf("|\t\tMenu Vetor                        |\n");
-    printf("===================================================\n");
+    linhap(51);
     printf("|1. Ler vetores.                                  |\n");
-    printf("|2. Ler outros vetores.                           |\n");
-    printf("|3. Somar os dois vetores.                        |\n");
-    printf("|4. Multiplicar um dos vetores por um escalar.    |\n");
-    printf("|5. Realizar o produto escalar entre dois vetores.|\n");
-    printf("|6. Voltar ao menu anterior.                      |\n\n");
-    printf("Opção desejada: ");
-    scanf("%d", &mv);
+    printf("|2. Somar os dois vetores.                        |\n");
+    printf("|3. Multiplicar um dos vetores por um escalar.    |\n");
+    printf("|4. Realizar o produto escalar entre dois vetores.|\n");
+    printf("|5. Voltar ao menu anterior.                      |\n");
+    linhap(51); printf("\n");
+    printf("Opção desejada: "); scanf("%d", &mv);
+
     switch (mv) {
         case 1: lervetor(&vector1, &vector2, &tam); menuvetor(); break;
-        case 2: lervetor(&vector1, &vector2, &tam); menuvetor(); break;
-        case 3: Somar(vector1, vector2, tam); break;
-        case 4: Mult(vector1, vector2, tam); break;
-        case 6: main(); break;
+        case 2: Somar(vector1, vector2, tam); menuvetor(); break;
+        case 3: Mult(vector1, vector2, tam); menuvetor(); break;
+        case 4: Prod2V(vector1, vector2, tam); menuvetor(); break;
+        case 5: main(); break;
         default: printf("Opção inválida... Tente uma das opções do menu!\n\n"); menuvetor();
     }
 }
--- Biblioteca/menuvetor.h
+++ Biblioteca/menuvetor.h
@@ -1,3 +1,11 @@
+/* #############################################################
+    TRABALHO DA CADEIRA DE INTRODUÇÃO A CIÊNCIA DA COMPUTAÇÃO
+         BIBLIOTECA DE MANIPULAÇÃO DE VETORES EM C
+            Autor: Raimundo Angeliano Gonçalves de Sousa
+               UNIVERSIDADE ESTADUAL DO CEÁRA
+                   FORTALEZA/CE - 2018
+################################################################*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,11 +14,25 @@ int menuvetor();
 int lervetor(int **v1, int **v2, int *t);
 int Somar(int *v1, int *v2, int t);
 int Mult(int *v1, int *v2, int t);
+int Prod2V(int *v1, int *v2, int t);
+int linhap(int l){
+    for(int i = 0; i < l; i++){
+        printf("=");
+    }
+    printf("\n");
+}
 
+int voltavet(){
+    int voltar;
+    printf("0 para voltar ao menu: ");
+    scanf("%d", &voltar);
+}
+
+
+// FUNÇÃO PARA LEITURA DE DOIS VETORES
 int lervetor(int **v1, int **v2, int *t){
 
     int a, i;
-
     printf("-------------------------\n");
     printf("Tamanho dos vetores: ");
     scanf("%d", t);
@@ -29,34 +51,71 @@ int lervetor(int **v1, int **v2, int *t){
     return 0;
 }
 
+
+// FUNÇÃO PARA SOMAR OS DOIS VETORES LIDOS NA FUNÇÃO lervetor();
 int Somar(int *v1, int *v2, int t){
     printf("\n");
+    int p;
     for (int i = 0; i < t; i++) printf("v1[%d] + v2[%d] = [%d]\n",v1[i], v2[i], v1[i]+v2[i]);
-    printf("\n\n\n");
+    printf("\n\n");
+    voltavet();
 }
 
+
+// FUNÇÃO PARA MULTIPLICAR POR UM ESCALAR
 int Mult(int *v1, int *v2, int t){
-  for (int i=0;i<t;i++){
-    printf("%d", v1[i]);
+    printf("\n");
+    printf("===========================\n");
+    printf("[1] Vetor1: ");
+    for (int i=0;i<t;i++){
+        printf("%d ", v1[i]);
   }
-  for (int i=0;i<t;i++){
-    printf("%d", v2[i]);
+    printf("\n");
+    printf("[2] Vetor2: ");
+    for (int i=0;i<t;i++){
+        printf("%d ", v2[i]);
   }
-  int escolha, escalar;
-  printf("[1] Vetor1\n");
-  printf("[2] Vetor2\n");
-  printf("Qual dos vetores você quer manipular? \n");
-  scanf("%d", &escolha);
+    printf("\n");
+    printf("===========================\n");
+    int escolha, escalar, voltar;
+    printf("\nQual dos vetores você quer multiplicar? ");
+    scanf("%d", &escolha);
   switch (escolha){
-    case 1:
-      printf("Qual o escalar? \n");
+    case 1:{
+      printf("\nQual o escalar?");
       scanf("%d", &escalar);
-      for(int i=0;i<t;i++) printf("[%d] x %d = %d\n", v1[i], escolha, v1[i]*escalar);
-    case 2:
-      printf("Qual o escalar? \n");
+      while(escalar != 0){
+          if (escalar == 0) break;
+          for(int i=0;i<t;i++) printf("v1[%d] x v2[%d] = [%d]\n", v1[i], escalar, v1[i]*escalar); break;
+      } voltavet(); break;
+
+  } break;
+    case 2:{
+      printf("\nQual o escalar? ");
       scanf("%d", &escalar);
-      for(int i=0;i<t;i++) printf("[%d] x %d = %d\n", v2[i], escolha, v2[i]*escalar);
-    default: printf("Opção inválida\n");
+      while(escalar != 0){
+          if (escalar == 0) break;
+          for(int i=0;i<t;i++) printf("v1[%d] x v2[%d] = [%d]\n", v2[i], escalar, v2[i]*escalar); break;
+      } voltavet(); break;
+
+  } break;
+    default: {
+        printf("==================================================\n");
+        printf("Opção inválida           \n"); voltavet();
+        printf("==================================================\n");
+    }
+
   }
 
+}
+
+
+// FUNÇÃO QUE RETORNA O PRODUTO ENTRE DOIS VETORES LIDOS PELA FUNÇÃO lervetor();
+int Prod2V(int *v1, int *v2, int t){
+    printf("============================\n");
+    for(int i = 0; i < t; i++){
+        printf("v1[%d] * v2[%d] = [%d]\n", v1[i], v2[i], v1[i]*v2[i]);
+    }
+    printf("============================\n");
+    voltavet();
 }
