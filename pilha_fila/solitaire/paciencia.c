b4577f5559c99959ffa590c360b1fd00961ac7fa
Angeliano <angelo.sousa@acensjr.com>
2019-09-03 17:22:53 -0300

    WP

--- pilha_fila/solitaire/paciencia.c
+++ pilha_fila/solitaire/paciencia.c
@@ -13,16 +13,16 @@ void shuffle(card *deck);
 void hand_out(stack_top *stack[], stack_top *stock, card *deck);
 void show_stack_game(stack_top *stack[], stack_top *stock);
 void show_stock(stack_top *stock);
+void move_card(stack_top *stack[], stack_top *stock);
 
-// void move_card();
 
 int main(){
 
     card deck[52];
     stack_top *stock = stack_create();
+    stack_top *discard = stack_create();
     stack_top *cards_on_game[7];
     
-    // stack_top *discard;
     // stack_top *cards_out[4];
 
     create_deck(deck);
@@ -31,21 +31,21 @@ int main(){
     show_stack_game(cards_on_game, stock);
     show_stock(stock);
     
+
+    while(!stack_is_empty(stock) && !stack_is_empty(discard)){
+        printf("Pop %d\n", stack_pop(stock));
+        printf("Pop %d\n", stack_pop(discard));
+    }
+
+    // for(int i = 0; i = 7; i++){
+    //     stack_destroy(cards_on_game[i]);
+    // }
+    
     return 0;
 }
 
-void shuffle(card *deck){
-    for (int i = 0; i < 52; i++){
-        int r = rand() % 52;
-
-        card temp = deck[i];
-        deck[i] = deck[r];
-        deck[r] = temp;
-	}
-}
-
 void create_deck(card *deck){
-    // srand(time(NULL)); // ATIVAR QUANDO FOR PRECISO EMBARALHAR !!!!!!!!!!!!!!!!!
+    srand(time(NULL)); // ATIVAR QUANDO FOR PRECISO EMBARALHAR !!!!!!!!!!!!!!!!!
     char num = 0;
     for(int i = 0; i < 52; i++){
         num++;
@@ -65,26 +65,39 @@ void create_deck(card *deck){
     }    
 }
 
+void shuffle(card *deck){
+    for (int i = 0; i < 52; i++){
+        int r = rand() % 52;
+
+        card temp = deck[i];
+        deck[i] = deck[r];
+        deck[r] = temp;
+	}
+}
+
 void hand_out(stack_top *stack[], stack_top *stock, card *deck){
     for(int i = 0; i < 7; i++){
         stack[i] = stack_create();
     }
+    int i = 0;
+    int turn_on = 0;
 
-    stack_push(stack[0], deck[0].number, deck[0].suit, deck[0].color, deck[0].turn_on);
-                           
-    for(int i = 1; i < 3; i++) stack_push(stack[1], deck[i].number, deck[i].suit, deck[0].color, deck[0].turn_on);
+    stack_push(stack[0], deck[0].number, deck[0].suit, deck[0].color, turn_on);
+                        
+    for(i = 1; i < 3; i++) stack_push(stack[1], deck[i].number, deck[i].suit, deck[i].color, turn_on);
     
-    for(int i = 3; i < 6; i++) stack_push(stack[2], deck[i].number, deck[i].suit, deck[0].color, deck[0].turn_on);
+    for(i = 3; i < 6; i++) stack_push(stack[2], deck[i].number, deck[i].suit, deck[i].color, turn_on);
     
-    for(int i = 6; i < 10; i++) stack_push(stack[3], deck[i].number, deck[i].suit, deck[0].color, deck[0].turn_on);
+    for(i = 6; i < 10; i++) stack_push(stack[3], deck[i].number, deck[i].suit, deck[i].color, turn_on);
     
-    for(int i = 10; i < 15; i++) stack_push(stack[4], deck[i].number, deck[i].suit, deck[0].color, deck[0].turn_on);
+    for(i = 10; i < 15; i++) stack_push(stack[4], deck[i].number, deck[i].suit, deck[i].color, turn_on);
     
-    for(int i = 15; i < 21; i++) stack_push(stack[5], deck[i].number, deck[i].suit, deck[0].color, deck[0].turn_on);
+    for(i = 15; i < 21; i++) stack_push(stack[5], deck[i].number, deck[i].suit, deck[i].color, turn_on);
     
-    for(int i = 21; i < 28; i++) stack_push(stack[6], deck[i].number, deck[i].suit, deck[0].color, deck[0].turn_on);
+    for(i = 21; i < 28; i++) stack_push(stack[6], deck[i].number, deck[i].suit, deck[i].color, turn_on);
     
-    for(int i = 28; i < 52; i++) stack_push(stock, deck[i].number, deck[i].suit, deck[0].color, deck[0].turn_on);
+    for(i = 28; i < 52; i++) stack_push(stock, deck[i].number, deck[i].suit, deck[i].color, turn_on);
+
 }
 
 void show_stack_game(stack_top *stack[], stack_top *stock){
@@ -124,9 +137,13 @@ void show_stock(stack_top *stock){
         printf("%c ", stock->head->color);
         printf("%d ", stock->head->turn_on);
         a++;
-        if(a % 5 == 0) printf("\n");
+        if(a % 12 == 0) printf("\n");
         stack_pop(stock);
     }
-    printf("\n\n");
+    printf("\n");
 
 }
+
+// void move_card(stack_top *stack[], stack_top *stock, stack_top *discard, stack_top *discard){
+
+// }
